d0caf592d3909f4a2fb0e8fd8672565db447aa4a
Pete Bevin <pete@petebevin.com>
2014-07-13 10:45:12 -0400

    Add pattern match functionality

--- search.js
+++ search.js
@@ -2,10 +2,10 @@ var async = require('async');
 var fs = require('fs');
 var http = require('http');
 
-async.waterfall([ readFile, toLines, mapWith(preprocess), serve ], errorHandler);
+async.waterfall([ readFile, toLines, map(preprocess), serve ], errorHandler);
 
 function readFile(cb) {
-  fs.readFile("/usr/share/dict/words", "utf8", cb);
+  fs.readFile(process.env.DICT || "/usr/share/dict/words", "utf8", cb);
 }
 
 function toLines(data, cb) {
@@ -14,14 +14,14 @@ function toLines(data, cb) {
 
 function errorHandler(err) { if(err) throw(err) }
 
-function mapWith(fn) {
+function map(fn) {
   return function(data, cb) {
     cb(null, data.map(fn));
   }
 }
 
 function preprocess(word) {
-  var letters = word.toLowerCase().replace(/[^a-z]/g, "");
+  var letters = toLetters(word);
   var sorted = letters.split('').sort().join('');
   return {
     word: word,
@@ -30,9 +30,65 @@ function preprocess(word) {
   };
 }
 
-function matchField(key, value) {
+function toLetters(word) {
+  return word.toLowerCase().replace(/[^a-z]/gi, "");
+}
+
+function serve(words) {
+  console.log(words.slice(0, 10));
+  http.createServer(function (req, res) {
+    var path = req.url;
+    var statusCode = 200;
+    var pipeline;
+
+    if (path.startsWith("/an/")) {
+      pipeline = [ anagram(path.substring(4)), findMatches ];
+    } else if (path.startsWith("/fw/")) {
+      pipeline = [ patternMatch(path.substring(4)), findMatches ];
+    } else {
+      pipeline = [ withError("Not found.") ]
+    }
+
+    async.waterfall(pipeline, function(err, statusCode, text) {
+      res.writeHead(statusCode, {'Content-Type': 'text/plain'});
+      res.end(text);
+    });
+  }).listen(3000, '127.0.0.1');
+
+  function anagram(pattern) {
+    var sortedLetters = preprocess(pattern).sorted;
+
+    return function(cb) {
+      cb(null, fieldEquals("sorted", sortedLetters));
+    }
+  }
+
+  function patternMatch(pattern) {
+    var pattern = pattern.replace(/[^\.a-z]/gi, "");
+    var regexp = new RegExp("^" + pattern + "$", "i");
+    return function(cb) {
+      cb(null, fieldMatches("letters", regexp));
+    }
+  }
+
+  function findMatches(matcher, cb) {
+    cb(null, 200, words.filter(matcher).map(pluck("word")).join("\n"));
+  }
+
+  function withError(text, cb) {
+    cb(null, 404, text);
+  }
+}
+
+function fieldEquals(key, value) {
   return function(word) {
     return word[key] == value;
+  }
+}
+
+function fieldMatches(key, regexp) {
+  return function(word) {
+    return word[key].match(regexp);
   }
 }
 
@@ -42,17 +98,6 @@ function pluck(key) {
   }
 }
 
-function serve(words) {
-  http.createServer(function (req, res) {
-    var word = req.url.replace("/", "");
-
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(lookup(word) + "\n");
-  }).listen(3000, '127.0.0.1');
-
-  function lookup(word) {
-    var sortedLetters = preprocess(word).sorted;
-    var matcher = matchField("sorted", sortedLetters);
-    return words.filter(matcher).map(pluck("word")).join("\n");
-  }
-}
+String.prototype.startsWith = function (str){
+  return this.indexOf(str) == 0;
+};
